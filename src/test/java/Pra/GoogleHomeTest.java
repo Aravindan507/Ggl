279813ef53602b3a85a9218a2cdd665c8fb5efee
Aravindan <aravindansrinivas96@gmail.com>
2021-05-03 22:08:44 +0530

    new push

--- src/test/java/Pra/GoogleHomeTest.java
+++ src/test/java/Pra/GoogleHomeTest.java
@@ -1,15 +1,16 @@
+package Pra;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class GoogleHome {
+public class GoogleHomeTest {
 
-	WebDriver driver;
+	
 	@Test
 	public void getHomePage() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\eclipse_work\\chromedriver.exe");
-		 driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		 
 		 driver.get("https://www.noon.com/uae-en/");
 		 driver.manage().window().maximize();
